2d29a72191d9590738413386a346cd5937b39474
MikleBoroda <mikle.kuznecov@gmail.com>
2021-12-13 00:46:16 +0300

    UItest

--- src/test/java/ui_redmine/UnconfirmedUser.java
+++ src/test/java/ui_redmine/UnconfirmedUser.java
@@ -14,25 +14,34 @@ import static org.testng.Assert.*;
 public class UnconfirmedUser extends BaseUITest {
     private User user;
 
-    @BeforeMethod
+    @BeforeMethod(description =
+            "1. Заведен пользователь в системе.\n" +
+                    "2. Пользователь не подтвержден администратором и не заблокирован")
     public void prepareFixtures() {
         user = new User() {{
             setStatus(Status.UNACCEPTED);
         }}.create();
+
         openBrowser();
         headerPage.loginButton.click();
         loginPage.login(user);
 
-
     }
 
-    @Test
+    @Test(description = "Авторизация неподтвержденным пользователем")
     public void unconfirmedUserTest() {
         WebElement flashError = browser.getDriver().findElement(By.xpath("//div[@id='content']//div[@id='flash_error']"));
 
+        //1. Отображается домашняя страница
         assertEquals(headerPage.homePage.getText(), "Домашняя страница");
+
+        //Отображается ошибка с текстом "Ваша учётная запись создана и ожидает подтверждения администратора."
         assertEquals(flashError.getText(), "Ваша учётная запись создана и ожидает подтверждения администратора.");
+
+        //В заголовке страницы не отображаются элементы "Моя страница"
         assertFalse(BrowserUtils.isElementCurrentlyDisplayed(headerPage.myPage));
+
+        //В заголовке страницы отображаются элементы "Войти", "Регистрация"
         assertTrue(headerPage.loginButton.isDisplayed());
         assertTrue(headerPage.registerButton.isDisplayed());
 
