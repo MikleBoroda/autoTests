f933989e8197104741dd89a54f8d1f4402e7ee25
MikleBoroda <mikle.kuznecov@gmail.com>
2021-12-13 01:09:52 +0300

    UItest

--- src/test/java/ui_redmine/SortingListOfUsersByUser.java
+++ src/test/java/ui_redmine/SortingListOfUsersByUser.java
@@ -15,7 +15,9 @@ import static redmine.utils.CompareUtils.*;
 public class SortingListOfUsersByUser extends BaseUITest {
 
 
-    @BeforeMethod
+    @BeforeMethod(description =
+            "1. Заведен пользователь в системе с правами администратора\n" +
+                    "2. Заведено несколько пользователей в системе")
     public void prepareFixtures() {
 
         User admin = new User() {{
@@ -36,25 +38,36 @@ public class SortingListOfUsersByUser extends BaseUITest {
 
     }
 
-    @Test
+    @Test(description = "6. Администрирование. Сортировка списка пользователей по пользователю")
     public void sortingListOfUsersByUserTest() {
+
+        //Отображается домашняя страница
         assertEquals(headerPage.homePage.getText(), "Домашняя страница");
 
+        //На главной странице нажать "Администрирование"
         headerPage.administration.click();
+        //На главной странице нажать "Администрирование"
         assertEquals(administrationPage.administrationTitle.getText(), "Администрирование");
 
+        //Выбрать из списка меню "Пользователи"
         administrationPage.users.click();
+        // Отображается таблица с пользователями
         assertTrue(userTablePage.tableUsers.isDisplayed());
 
 
         List<String> userNameAsc = BrowserUtils.getElementsText(userTablePage.userName);
+
+        // Таблица с пользователями отсортирована по логину пользователей по возрастанию
         assertListSortedByUserNameAsc(userNameAsc);
 
+        //В шапке таблицы нажать на "Пользователь"
         userTablePage.button("Пользователь").click();
 
-        List<String> userNameDesc = BrowserUtils.getElementsText(userTablePage.userName);
-        assertListSortedByUserNameDesc(userNameDesc);
 
+        List<String> userNameDesc = BrowserUtils.getElementsText(userTablePage.userName);
+
+        //Таблица с пользователями отсортирована по логину пользователей по убыванию
+        assertListSortedByUserNameDesc(userNameDesc);
 
     }
 
