32a61c250a6af568221f64b62b509662469d1e2e
MikleBoroda <mikle.kuznecov@gmail.com>
2021-12-13 00:53:43 +0300

    UItest

--- src/test/java/ui_redmine/ProjectVisibilityPrivate.java
+++ src/test/java/ui_redmine/ProjectVisibilityPrivate.java
@@ -13,18 +13,18 @@ public class ProjectVisibilityPrivate extends BaseUITest {
     private User admin;
     private Project projectNotTied;
 
-    @BeforeMethod
+    @BeforeMethod(description =
+            "1. Заведен пользователь в системе с правами администратора\n" +
+                    "2. Существует приватный проект, не привязанный к пользователю")
     public void prepareFixtures() {
-
-
         admin = new User() {{
             setIsAdmin(true);
-
         }}.create();
 
         projectNotTied = new Project() {{
             setIsPublic(false);
         }}.create();
+
 
         openBrowser();
         headerPage.loginButton.click();
@@ -34,10 +34,16 @@ public class ProjectVisibilityPrivate extends BaseUITest {
 
     @Test
     public void privateProjectTest() {
-
+        //1. Отображается домашняя страница
         assertEquals(headerPage.homePage.getText(), "Домашняя страница");
+
+        //2. На главной странице нажать "Проекты"
         headerPage.projects.click();
+
+        //Отображается страница "Проекты"
         assertEquals(projectsPage.projectsTitle.getText(), "Проекты");
+
+        //На странице отображается проект из предусловия
         assertEquals(projectNotTied.getName(), BrowserUtils.checkProject(projectNotTied.getName()).getText());
 
 
