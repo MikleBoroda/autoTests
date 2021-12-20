795830bad941d9daf85e9ad4cf687ec3be782fd7
MikleBoroda <mikle.kuznecov@gmail.com>
2021-12-21 01:02:34 +0300

    Allure

--- src/main/java/redmine/utils/CompareUtils.java
+++ src/main/java/redmine/utils/CompareUtils.java
@@ -1,6 +1,10 @@
 package redmine.utils;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import org.testng.Assert;
+import redmine.allure.asserts.AllureAssert;
+
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -8,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+import static redmine.allure.asserts.AllureAssert.*;
 
 public class CompareUtils {
 
@@ -22,63 +28,70 @@ public class CompareUtils {
     private static final Comparator<String> USER_DESC_COMPARATOR = (s1, s2) -> s2.compareToIgnoreCase(s1);
     private static final Comparator<String> USER_ASC_COMPARATOR = USER_DESC_COMPARATOR.reversed();
 
-    // сортировка дата создания по возрастанию
+    @Step("Проверка сортировки списка дат по убыванию")
     public static void assertListSortedByDateDesc(List<String> dates) {
         List<String> sortedDates = new ArrayList<>(dates);
         sortedDates.sort(DATE_DESC_COMPARATOR);
-        Assert.assertEquals(dates, sortedDates);
+        assertEquals(dates, sortedDates);
 
     }
 
-    // сортировка дата создания по убыванию
+    @Step("Проверка сортировки списка дат по возрастанию")
     public static void assertListSortedByDateAsc(List<String> dates) {
         List<String> sortedDates = new ArrayList<>(dates);
         sortedDates.sort(DATE_ASC_COMPARATOR);
-        Assert.assertEquals(dates, sortedDates);
+        assertEquals(dates, sortedDates);
     }
 
-    // сортировка логина по убыванию
+
+    @Step("Проверка сортировки логина по убыванию")
     public static void assertListSortedByUserNameDesc(List<String> userName) {
         List<String> sortedUserNAme = new ArrayList<>(userName);
         sortedUserNAme.sort(USER_DESC_COMPARATOR);
-        Assert.assertEquals(userName, sortedUserNAme);
+        AllureAssert.assertEquals(userName, sortedUserNAme);
     }
 
-    // сортировка логина по возрастанию
+
+    @Step("Проверка сортировки логина по возрастанию")
     public static void assertListSortedByUserNameAsc(List<String> userName) {
         List<String> sortedUserNAme = new ArrayList<>(userName);
         sortedUserNAme.sort(USER_ASC_COMPARATOR);
-        Assert.assertEquals(userName, sortedUserNAme);
+        AllureAssert.assertEquals(userName, sortedUserNAme);
     }
 
-    // сортировка по имени пользователя по убыванию
+
+    @Step("Проверка сортировки имени пользователя по убыванию")
     public static void assertListSortedByFirstNameDesc(List<String> firstName) {
         List<String> sortedFirstName = new ArrayList<>(firstName);
         sortedFirstName.sort(USER_DESC_COMPARATOR);
-        Assert.assertEquals(firstName, sortedFirstName);
+        AllureAssert.assertEquals(firstName, sortedFirstName);
     }
 
-    // сортировка по имени пользователя по возрастанию
+
+    @Step("Проверка сортировки имени пользователя по возрастанию")
     public static void assertListSortedByFirstNameAsc(List<String> firstName) {
         List<String> sortedFirstName = new ArrayList<>(firstName);
         sortedFirstName.sort(USER_ASC_COMPARATOR);
-        Assert.assertEquals(firstName, sortedFirstName);
+        AllureAssert.assertEquals(firstName, sortedFirstName);
     }
 
-    // сортировка по фамили пользователя по убыванию
+
+    @Step("Проверка сортировки фамилии пользователя по убыванию")
     public static void assertListSortedByLastNameDesc(List<String> lastName) {
         List<String> sortedLastName = new ArrayList<>(lastName);
         sortedLastName.sort(USER_DESC_COMPARATOR);
-        Assert.assertEquals(lastName, sortedLastName);
+        AllureAssert.assertEquals(lastName, sortedLastName);
     }
 
-    // сортировка по фамили пользователя по возрастанию
+
+    @Step("Проверка сортировки фамилии пользователя по возрастанию")
     public static void assertListSortedByLastNameAsc(List<String> lastName) {
         List<String> sortedLastName = new ArrayList<>(lastName);
         sortedLastName.sort(USER_ASC_COMPARATOR);
-        Assert.assertEquals(lastName, sortedLastName);
+        AllureAssert.assertEquals(lastName, sortedLastName);
     }
 
+    @Step("Проверка сортировки списка")
     public static Boolean checkSortedList(List<String> list) {
         List<String> listSorted = new ArrayList<>(list);
         Collections.sort(listSorted);
